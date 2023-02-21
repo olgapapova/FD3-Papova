7c845477b551a0d73d03eaee5c30d78328f3e18c
olga_papova <olena89-gulena@mail.ru>
2023-02-22 00:13:56 +0300

    ishop2

---
+++ ishop2/Product.js
@@ -0,0 +1,49 @@
+var Product = React.createClass({
+
+    displayName: 'Product',
+  
+    propTypes: {
+        code: React.PropTypes.number.isRequired,
+        count: React.PropTypes.number.isRequired,
+        title: React.PropTypes.string.isRequired,
+        price: React.PropTypes.string.isRequired,
+        foto: React.PropTypes.string,
+        cbProductSelect:React.PropTypes.func.isRequired,
+        cbProductSelectDel:React.PropTypes.func.isRequired,
+        productSelectedCod:React.PropTypes.number,
+        productSelectedCodDel:React.PropTypes.number,
+    },
+
+    productClicked: function(EO) {
+      this.props.cbProductSelect(this.props.code);
+      console.log("product: "+this.props.code+" "+this.props.productSelectedCod);
+    },
+
+    productDel: function(EO) {
+      this.props.cbProductSelectDel(this.props.code);
+      console.log("product: "+this.props.code);
+    },
+  
+    render: function() {
+
+      if (this.props.productSelectedCodDel===this.props.code) {
+        return null
+      }
+      else {
+        return React.DOM.tr((this.props.code===this.props.productSelectedCod)?{className:'LineCl',
+        onClick:this.productClicked}:{className:'Line',onClick:this.productClicked},
+          React.DOM.td({className:'Product'},this.props.title),
+          React.DOM.td({className:'Line'},this.props.price),
+          React.DOM.td({className:'Line'},
+          React.DOM.img({className:'Image', src: this.props.foto})
+        ),
+        React.DOM.td({className:'Line'},this.props.count),
+        React.DOM.td({className:'Button'},
+          React.DOM.input({type:'button',value:'Delete',onClick:this.productDel},
+          ),
+        )
+        )
+      }
+    },
+  
+  });
---
+++ ishop2/Shop.js
@@ -0,0 +1,79 @@
+var Shop = React.createClass({
+
+    displayName: 'Shop',
+  
+    propTypes: {
+      storeName: React.PropTypes.string.isRequired, // название магазина
+      productsList:React.PropTypes.arrayOf(    // список продуктов
+         React.PropTypes.shape({
+           code: React.PropTypes.number.isRequired,
+           count: React.PropTypes.number.isRequired,
+           title: React.PropTypes.string.isRequired,
+           price: React.PropTypes.string.isRequired,
+           foto: React.PropTypes.string,
+         })
+       ),
+       columnName:React.PropTypes.arrayOf(    // шапка таблицы
+       React.PropTypes.shape({
+         codeZ: React.PropTypes.number.isRequired,
+         countZ: React.PropTypes.string.isRequired,
+         titleZ: React.PropTypes.string.isRequired,
+         priceZ: React.PropTypes.string.isRequired,
+         fotoZ: React.PropTypes.string.isRequired,
+         control: React.PropTypes.string.isRequired,
+       })
+     )
+    },
+
+    getInitialState: function() {
+      return { 
+        productSelectedCode: null,
+        productSelectedCodeDel: null,
+      };
+    },
+
+    productSelected: function(code) {
+      this.setState( {productSelectedCode:code}, ()=> {console.log (this.state.productSelectedCode)});
+    },
+
+    productDelete: function(code) {
+      this.setState( {productSelectedCodeDel:code}, ()=> {console.log (this.state.productSelectedCodeDel)});
+    },
+  
+    render: function() {
+
+      var culumnName=this.props.columnName.map( v =>
+        React.DOM.tr({className:'LineZ',key:v.codeZ},
+          React.DOM.th( {className:'LineZ'}, v.titleZ),
+          React.DOM.th( {className:'LineZ'}, v.priceZ),
+          React.DOM.th( {className:'LineZ'}, v.fotoZ),
+          React.DOM.th( {className:'LineZ'}, v.countZ),
+          React.DOM.th( {className:'LineZ'}, v.control),
+        ),
+      );
+
+      var cellsContents=this.props.productsList.map( v =>
+        React.createElement(Product,{key:v.code,
+          count:v.count,
+          title:v.title,
+          price:v.price,
+          code:v.code,
+          foto:v.foto,
+          cbProductSelect:this.productSelected,
+          cbProductSelectDel:this.productDelete,
+          productSelectedCod:this.state.productSelectedCode,
+          productSelectedCodDel:this.state.productSelectedCodeDel,
+        }),
+        );
+      return React.DOM.table( {className:'Shop'},
+        React.DOM.caption( {className:'NameStore'}, this.props.storeName ),
+        React.DOM.thead( null),
+        React.DOM.tbody( null),
+        React.DOM.tfoot( {className:'Foot'}, 
+           culumnName,
+          cellsContents
+        ),
+      );
+    },
+  
+  });
